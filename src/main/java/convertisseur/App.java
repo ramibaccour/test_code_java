e34a76adb0024c311a20497a5b374bf40d5b7a80
ramibaccour <48089762+ramibaccour@users.noreply.github.com>
2023-09-16 09:27:56 +0100

    create new project by Vscode (mvn project)

---
+++ src/main/java/convertisseur/App.java
@@ -0,0 +1,13 @@
+package convertisseur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
