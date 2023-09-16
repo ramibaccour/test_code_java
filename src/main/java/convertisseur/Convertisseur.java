89949ff8a5adbb2dca4a5503aa053745a15bf4c9
ramibaccour <48089762+ramibaccour@users.noreply.github.com>
2023-09-16 11:17:18 +0100

    création des tableau des chiffres romains

--- src/main/java/convertisseur/Convertisseur.java
+++ src/main/java/convertisseur/Convertisseur.java
@@ -4,6 +4,10 @@ public class Convertisseur
 {
     public String convertToRomain(Integer number)
     {
+        final String [] tabUnites = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        final String [] tabDizaines = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        final String [] tabCentaines = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        final String [] tabMilliers = {"","M","MM","MMM","MMMM","MMMMM","MMMMMM","MMMMMMM" };
         return number.toString();
     }
 
--- src/test/java/convertisseur/AppTest.java
+++ src/test/java/convertisseur/AppTest.java
@@ -3,6 +3,7 @@ package convertisseur;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -18,6 +19,7 @@ public class AppTest
         convertisseur = new Convertisseur();
 	}
 
+    @DisplayName("Réussir à convertir un chiffre arabe => romain")
     @ParameterizedTest(name = "{0} should equal to {1}")
 	@CsvSource({ "1,I", "2,II", "3,III" })
     public void test_convert_int_to_romain(Integer number, String numberRomain)
