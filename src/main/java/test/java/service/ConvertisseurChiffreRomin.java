eaecac533e964302e6eb7a6d80dc8e1c7dbbdb18
ramibaccour <48089762+ramibaccour@users.noreply.github.com>
2023-09-16 07:56:27 +0100

    creat solution sans sring boot

--- src/main/java/test/java/service/ConvertisseurChiffreRomin.java
+++
@@ -1,9 +0,0 @@
-package test.java.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ConvertisseurChiffreRomin 
-{
-    public String convertisseurChiffre(Integer chiffre);
-}
